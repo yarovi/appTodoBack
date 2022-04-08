b5b6b588fc011890e73cab045f3df2fafe51d80a
Yasmani Rosado <tas185@gmail.com>
2022-04-08 07:29:11 -0500

    config base

---
+++ appTodoBack/src/main/java/com/apptodo/demo/AppTodoBackApplication.java
@@ -0,0 +1,13 @@
+package com.apptodo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTodoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTodoBackApplication.class, args);
+	}
+
+}
